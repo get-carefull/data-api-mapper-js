aed5e57a53bbe5bdbd465e19b119ddaad00daa4d
leandro salo <leito.salo@gmail.com>
2021-06-13 21:05:34 -0300

    feat: returning metadata rows and objects as result

--- src/outputDataApiBuilder/QueryResponse.js
+++ src/outputDataApiBuilder/QueryResponse.js
@@ -1,28 +1,61 @@
-const {QueryMetadata} = require("./QueryMetadata");
-
 class QueryResponse {
 
     constructor() {
+        this.rows = []
+        this.items = []
     }
 
     parse(response) {
-        const rows = []
         const metadata = response.columnMetadata
-        response.records.forEach((row, index) => {
-            const item = []
-            row.forEach((col, index) => {
-                item.push({
-                    name: metadata[index].name,
-                    value: Object.entries(col)[0][1],
+        response.records.forEach(record => {
+            const row = []
+            const item = {}
+            record.forEach((col, index) => {
+                const typeDataApi = metadata[index].typeName
+                const colName = metadata[index].name
+                row.push({
+                    name: colName,
+                    value: col.isNull ? null : Object.entries(col)[0][1],
                     tableName: metadata[index].tableName,
                     typeDbName: metadata[index].typeName,
                     nullable: metadata[index].nullable !== 0,
-                    typeDataApi: Object.entries(col)[0].key
+                    typeDataApi: typeDataApi
                 })
+
+                // this can be returned on demand with a flag
+                const attr = {}
+                attr[colName] = col.isNull ? null : parserValueFromType(Object.entries(col)[0][1], typeDataApi)
+                Object.assign(item, attr)
             })
-            rows.push(item)
+            this.items.push(item)
+            this.rows.push(row)
         })
-        return new QueryMetadata(rows)
+        return this
+    }
+}
+
+// postgresql specific
+function parserValueFromType(value, type) {
+    switch (type) {
+        case 'text':
+        case 'serial':
+        case 'bool':
+            return value
+        case 'int4':
+            return parseInt(value)
+        case 'jsonb':
+            return JSON.parse(value)
+        case 'numeric':
+        case 'float8':
+            return parseFloat(value)
+        case 'timestamptz':
+        case 'timestamp':
+            return new Date(value)
+        case 'date':
+            // TODO: JS doesn't have a Date type (without time), so for now it delegates to the user what to do with it
+            return value
+        default:
+            return value
     }
 }
 
--- test/DataApliClientTest.js
+++ test/DataApliClientTest.js
@@ -9,7 +9,7 @@ describe('DataApiClientTest', () => {
     const region = 'us-east-1'
     const dataApiClient = new DataApiClient(secretArn, resourceArn, database, null, region)
 
-    before(async function() {
+    before(async function () {
         const dropTable = 'DROP TABLE IF EXISTS aurora_data_api_node_test'
         const createTable = 'CREATE TABLE aurora_data_api_node_test (id SERIAL, a_name TEXT, doc JSONB DEFAULT \'{}\', num_numeric NUMERIC (10, 5) DEFAULT 0.0, num_float float, num_integer integer, ts TIMESTAMP WITH TIME ZONE, field_string_null TEXT NULL, field_long_null integer NULL, field_doc_null JSONB NULL, field_boolean BOOLEAN NULL, tz_notimezone TIMESTAMP, a_date DATE);'
         const firstInsert = "INSERT INTO aurora_data_api_node_test (a_name, doc, num_numeric, num_float, num_integer, ts, tz_notimezone, a_date) VALUES ('first row', '{\"string_vale\": \"string1\", \"int_value\": 1, \"float_value\": 1.11}', 1.12345, 1.11, 1, '1976-11-02 08:45:00 UTC', '2021-03-03 15:51:48.082288', '1976-11-02');"
@@ -20,27 +20,19 @@ describe('DataApiClientTest', () => {
         await dataApiClient.query(secondInsert)
     })
 
-    after(async function() {
+    after(async function () {
         await dataApiClient.query('DROP TABLE IF EXISTS aurora_data_api_node_test')
     })
 
-    describe('DataApiClientExecute', function() {
+    describe('DataApiClientExecute', function () {
         it('Obtain response OK', async () => {
             const response = await dataApiClient.query('SELECT * FROM aurora_data_api_node_test where id=:id', {id: 1})
-            const resultExpected = [{"name": "id","tableName": "aurora_data_api_node_test","typeDbName": "serial","nullable": false,"typeDataApi": "longValue","value": 1},
-                {"name": "a_name","tableName": "aurora_data_api_node_test","typeDbName": "text","nullable": true,"typeDataApi": "stringValue","value": "first row"},
-                {"name": "doc","tableName": "aurora_data_api_node_test","typeDbName": "jsonb","nullable": true,"typeDataApi": "stringValue","value": "{\"int_value\": 1, \"float_value\": 1.11, \"string_vale\": \"string1\"}"},
-                {"name": "num_numeric","tableName": "aurora_data_api_node_test","typeDbName": "numeric","nullable": true,"typeDataApi": "stringValue","value": "1.12345"},
-                {"name": "num_float","tableName": "aurora_data_api_node_test","typeDbName": "float8","nullable": true,"typeDataApi": "doubleValue","value": 1.11},
-                {"name": "num_integer","tableName": "aurora_data_api_node_test","typeDbName": "int4","nullable": true,"typeDataApi": "longValue","value": 1},
-                {"name": "ts","tableName": "aurora_data_api_node_test","typeDbName": "timestamptz","nullable": true,"typeDataApi": "stringValue","value": "1976-11-02 08:45:00"},
-                {"name": "field_string_null","tableName": "aurora_data_api_node_test","typeDbName": "text","nullable": true,"typeDataApi": "isNull","value": true},
-                {"name": "field_long_null","tableName": "aurora_data_api_node_test","typeDbName": "int4","nullable": true,"typeDataApi": "isNull","value": true},
-                {"name": "field_doc_null","tableName": "aurora_data_api_node_test","typeDbName": "jsonb","nullable": true,"typeDataApi": "isNull","value": true},
-                {"name": "field_boolean","tableName": "aurora_data_api_node_test","typeDbName": "bool","nullable": true,"typeDataApi": "isNull","value": true},
-                {"name": "tz_notimezone","tableName": "aurora_data_api_node_test","typeDbName": "timestamp","nullable": true,"typeDataApi": "stringValue","value": "2021-03-03 15:51:48.082288"},
-                {"name": "a_date","tableName": "aurora_data_api_node_test","typeDbName": "date","nullable": true,"typeDataApi": "stringValue","value": "1976-11-02"}]
-            assert.strictEqual(JSON.stringify(response.getRows()), JSON.stringify(resultExpected))
+            const resultRowsExpected = [[{"name":"id","value":1,"tableName":"aurora_data_api_node_test","typeDbName":"serial","nullable":false,"typeDataApi":"serial"},{"name":"a_name","value":"first row","tableName":"aurora_data_api_node_test","typeDbName":"text","nullable":true,"typeDataApi":"text"},{"name":"doc","value":"{\"int_value\": 1, \"float_value\": 1.11, \"string_vale\": \"string1\"}","tableName":"aurora_data_api_node_test","typeDbName":"jsonb","nullable":true,"typeDataApi":"jsonb"},{"name":"num_numeric","value":"1.12345","tableName":"aurora_data_api_node_test","typeDbName":"numeric","nullable":true,"typeDataApi":"numeric"},{"name":"num_float","value":1.11,"tableName":"aurora_data_api_node_test","typeDbName":"float8","nullable":true,"typeDataApi":"float8"},{"name":"num_integer","value":1,"tableName":"aurora_data_api_node_test","typeDbName":"int4","nullable":true,"typeDataApi":"int4"},{"name":"ts","value":"1976-11-02 08:45:00","tableName":"aurora_data_api_node_test","typeDbName":"timestamptz","nullable":true,"typeDataApi":"timestamptz"},{"name":"field_string_null","value":null,"tableName":"aurora_data_api_node_test","typeDbName":"text","nullable":true,"typeDataApi":"text"},{"name":"field_long_null","value":null,"tableName":"aurora_data_api_node_test","typeDbName":"int4","nullable":true,"typeDataApi":"int4"},{"name":"field_doc_null","value":null,"tableName":"aurora_data_api_node_test","typeDbName":"jsonb","nullable":true,"typeDataApi":"jsonb"},{"name":"field_boolean","value":null,"tableName":"aurora_data_api_node_test","typeDbName":"bool","nullable":true,"typeDataApi":"bool"},{"name":"tz_notimezone","value":"2021-03-03 15:51:48.082288","tableName":"aurora_data_api_node_test","typeDbName":"timestamp","nullable":true,"typeDataApi":"timestamp"},{"name":"a_date","value":"1976-11-02","tableName":"aurora_data_api_node_test","typeDbName":"date","nullable":true,"typeDataApi":"date"}]]
+            const resultItemsExpected = [{"id":1,"a_name":"first row","doc":{"int_value":1,"float_value":1.11,"string_vale":"string1"},"num_numeric":1.12345,"num_float":1.11,"num_integer":1,"ts":"1976-11-02T11:45:00.000Z","field_string_null":null,"field_long_null":null,"field_doc_null":null,"field_boolean":null,"tz_notimezone":"2021-03-03T18:51:48.082Z","a_date":"1976-11-02"}]
+            assert.strictEqual(JSON.stringify(response.rows), JSON.stringify(resultRowsExpected))
+            assert.strictEqual(JSON.stringify(response.items), JSON.stringify(resultItemsExpected))
         })
     })
 })
+
+
